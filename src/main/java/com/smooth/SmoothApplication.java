2831b2520eab03ad0a415fba460445aa03c7c98a
Warawat Suksakulwat <Warawat.Suksakulwat@refinitiv.com>
2021-01-21 13:18:27 +0700

    Add Log

--- src/main/java/com/smooth/SmoothApplication.java
+++ src/main/java/com/smooth/SmoothApplication.java
@@ -1,5 +1,7 @@
 package com.smooth;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SmoothApplication {
 
+    private static final Logger logger = LogManager.getLogger(SmoothApplication.class);
     public static void main(String[] args) {
+        logger.info("TEST");
         SpringApplication.run(SmoothApplication.class, args);
     }
 
